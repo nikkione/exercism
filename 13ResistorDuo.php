bb9aab9bc7aa51c97a685df1ba257f9552a7c2d8
Nikita Andreevich <xniki4x@gmail.com>
2024-01-17 18:28:37 +0300

    solution with loop

---
+++ 13ResistorDuo.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Resistor Color Duo
+Instructions
+If you want to build something using a Raspberry Pi, you'll probably use resistors. For this exercise, you need to know two things about them:
+
+Each resistor has a resistance value.
+Resistors are small - so small in fact that if you printed the resistance value on them, it would be hard to read.
+To get around this problem, manufacturers print color-coded bands onto the resistors to denote their resistance values. Each band has a position and a numeric value.
+
+The first 2 bands of a resistor have a simple encoding scheme: each color maps to a single number. For example, if they printed a brown band (value 1) followed by a green band (value 5), it would translate to the number 15.
+
+In this exercise you are going to create a helpful program so that you don't have to remember the values of the bands. The program will take color names as input and output a two digit number, even if the input is more than two colors!
+
+The band colors are encoded as follows:
+
+Black: 0
+Brown: 1
+Red: 2
+Orange: 3
+Yellow: 4
+Green: 5
+Blue: 6
+Violet: 7
+Grey: 8
+White: 9
+From the example above: brown-green should return 15 brown-green-violet should return 15 too, ignoring the third color.
+*/
+
+<?php
+
+/*
+ * By adding type hints and enabling strict type checking, code can become
+ * easier to read, self-documenting and reduce the number of potential bugs.
+ * By default, type declarations are non-strict, which means they will attempt
+ * to change the original type to match the type specified by the
+ * type-declaration.
+ *
+ * In other words, if you pass a string to a function requiring a float,
+ * it will attempt to convert the string value to a float.
+ *
+ * To enable strict mode, a single declare directive must be placed at the top
+ * of the file.
+ * This means that the strictness of typing is configured on a per-file basis.
+ * This directive not only affects the type declarations of parameters, but also
+ * a function's return type.
+ *
+ * For more info review the Concept on strict type checking in the PHP track
+ * <link>.
+ *
+ * To disable strict typing, comment out the directive below.
+ */
+
+declare(strict_types=1);
+
+class ResistorColorDuo
+{
+    public function getColorsValue(array $colors): int
+    {
+        $array = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"] ;
+        $string = "";
+        $i=0;
+        foreach($colors as $color) {
+            $string .= array_search($color, $array);
+            $i++;
+            if ($i == 2){
+                break;
+            }
+        }
+        return (int) $string;
+    }
+}
